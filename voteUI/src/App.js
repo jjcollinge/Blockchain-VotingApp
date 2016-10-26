0ea5d26d92ca83480b277d457ab0e393e42d7663
dotjson <jonathancollinge@live.com>
2016-10-26 09:45:39 +0000

    Started adding voting logic to client

--- voteUI/src/App.js
+++ voteUI/src/App.js
@@ -3,16 +3,85 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: "Blockchain Vote",
+      optionA: "loading...",
+      optionAEnabled: false,
+      optionB: "loading...",
+      optionBEnabled: false,
+      showResults: false,
+      results: "Results will be displayed when voting is closed.",
+      proposalIndex: 0
+    }
+  }
+
+  updateProposal() {
+    var proposals = [{
+      _optionA: "Alice",
+      _optionB: "Lucy"
+    },{
+      _optionA: "Bryan",
+      _optionB: "Matt"
+    }]
+
+    var proposal = proposals[this.state.proposalIndex];
+
+    // Gets current proposal
+    this.setState({
+      optionA: proposal._optionA,
+      optionAEnabled: true,
+      optionB: proposal._optionB,
+      optionBEnabled: true,
+    })
+  }
+
+  submitVote(option) {
+    console.log(option);
+  }
+
+  getResults() {
+    // While no results
+    setTimeout(function() {
+      this.setState({
+        results: "OptionA: 10, OptionB: 20",
+        showResults: true
+      })
+    }, 1000);
+  }
+
+  handleClick(option) {
+    this.submitVote(option);
+    this.setState({optionAEnabled: false,
+                   optionBEnabled: false,
+                   showResults: true});
+    // Wait for voting to end
+    // Display results
+    this.getResults();
+    // Wait for next proposal
+    // updateProposal
+  }
+
+  componentWillMount() {
+    this.updateProposal();
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>{this.state.title}</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-proposal">
+          <button className="App-optionA-btn" disabled={!this.state.optionAEnabled} onClick={() => this.handleClick("A")}>{this.state.optionA}</button>
+          <p className="vs">vs</p>
+          <button className="App-optionB-btn" disabled={!this.state.optionBEnabled} onClick={() => this.handleClick("B")}>{this.state.optionB}</button>
+        </div>
+        <div className={this.state.showResults ? 'App-results' : 'App-results-hidden'}>
+          <p className="App-results-text">{this.state.results}</p>
+        </div>
       </div>
     );
   }
